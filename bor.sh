a7ae2b642de9f47f7eee39ceeb19b41af3ed442b
Jordan Hagan <jordan@ephess.nz>
2022-12-20 13:08:10 +1100

    point install script to PFL releases

--- bor.sh
+++ bor.sh
@@ -82,7 +82,7 @@ else
     tag=${version}
 fi
 
-baseUrl="https://github.com/maticnetwork/bor/releases/download/v${version}"
+baseUrl="https://github.com/Polygon-Fast-Lane/bor/releases/download/v${version}"
 
 echo $baseUrl
 
